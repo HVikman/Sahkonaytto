323629c5f47d5a6558cb578fe0dd51053562a02f
jormaliisa <vikmanhenri95@gmail.com>
2022-12-14 22:43:39 +0200

    lisätty oled näyttö, vaihdettu asetukset json tiedostoon ja avattu mahdollisuus muillekkin asetuksille

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-# A simple example that:
-# - Connects to a WiFi Network defined by "ssid" and "password"
-# - Performs a GET request (loads a webpage)
-# - Queries the current time from a server
-
-import uos
+from uos import stat
 import network   # handles connecting to WiFi
 import urequests # handles making and servicing network requests
-from machine import Pin
+from machine import Pin, I2C 
 import json
 import time
+import sh1106
+
 
 led1 = Pin(9, Pin.OUT)
 led2 = Pin(10, Pin.OUT)
@@ -17,10 +14,11 @@ led4 = Pin(12, Pin.OUT)
 led5 = Pin(13, Pin.OUT)
 led6 = Pin(14, Pin.OUT)
 led7 = Pin(15, Pin.OUT)
-
-button = Pin(16, Pin.IN, Pin.PULL_DOWN)
+settingsmode=False
+button = Pin(17, Pin.IN, Pin.PULL_DOWN)
 holdsec = 0
-
+i2c = I2C(0,scl=Pin(1), sda=Pin(0), freq=400000)
+display = sh1106.SH1106_I2C(128, 64, i2c, Pin(29), 0x3c,rotate=180)
 while (button.value()==1):
     holdsec += 1
     led1.value(1)
@@ -29,29 +27,47 @@ while (button.value()==1):
     led1.value(0)
     led7.value(0)
     time.sleep(0.5)
-    if(holdsec==30):
+    if(holdsec==5):
         print("resetoidaan")
-        wlansettings = open("settings.csv","w")
-        wlansettings.close()
+        settingsmode=True
         break
         
-
-
-
 try:
-    filesize = uos.stat("settings.csv")[6]
+    if(stat("settings.json")):
+        fileok=True
 except OSError:
-    wlansettings = open("settings.csv","w")
-    wlansettings.close()
-    filesize = uos.stat("settings.csv")[6]
+    fileok=False
+
+if(fileok==True):
+    a_file = open("settings.json", "r")
+    settings = json.load(a_file)
+    a_file.close()
+    print(settings)
+else:
+    settings ={
+    "ssid":"",
+    "password":"",
+    }
+    a_file = open("settings.json","w")
+    json.dump(settings,a_file)
+    a_file.close()
+
+if(len(settings["password"]) > 0):
+    wlansettings=False
+
+else:
+    wlansettings=True
 
 
-if(filesize>0):
+
+
+if(settingsmode==False and wlansettings==False):
     attempts=0
-    wlansettings = open("settings.csv","r")
-    wlaninfo = wlansettings.read()
-    splitinfo = wlaninfo.split(",")
-    wlansettings.close()
+    a_file = open("settings.json", "r")
+    settings = json.load(a_file)
+    a_file.close()
+    
+
 
 
 
@@ -59,13 +75,17 @@ if(filesize>0):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
 
-    wlan.connect(splitinfo[0], splitinfo[1])
+    wlan.connect(settings["ssid"], settings["password"])
     while wlan.isconnected() == False:
         print('Waiting for connection...')
+        display.sleep(False)
+        display.fill(0)
+        display.text('Connecting...', 0, 0, 1)
+        display.show()
         time.sleep(1)
         attempts += 1
-        if(attempts > 19):
-            wlansettings = open("settings.csv","w")
+        if(attempts > 29):
+            wlansettings = open("settings.json","w")
             wlansettings.close()
             machine.reset()
             
@@ -86,7 +106,12 @@ if(filesize>0):
         raver = round(float(average))
         rounded = round(float(price))
         print(rounded)
-        
+        display.fill(0)
+        display.text("Hinta on nyt: ", 0, 0, 1)
+        display.text(str(rounded)+"snt/kwh", 0, 8, 1)
+        display.text("Keskihinta: ", 0, 16, 1)
+        display.text(str(raver)+"snt/kwh", 0, 24, 1)
+        display.show()
         return
 
     def leds(hinta):
@@ -100,8 +125,11 @@ if(filesize>0):
         return
 
     getPrices()
+
     nextTime = time.time()+300
     print(nextTime)
+
+
     while True:
         if (nextTime < time.time()):
             getPrices()
@@ -113,7 +141,7 @@ if(filesize>0):
                 leds(raver)
                 time.sleep(10)
     
-else:
+elif(wlansettings==True or settingsmode==True):
     import socket
     def connect():
         ssid = "PicoW"
@@ -168,16 +196,17 @@ else:
     
     def parsestring(request):
         
-        wlansettings = open("settings.csv","w")
         ssid = request.split("&")[0]
         ssid = ssid.split("=")[1]
         password = request.split("&")[1]
         password = password.split("=")[1]
         
-        print(ssid + ' , ' + password)
-
-        wlansettings.write(ssid+"," +password)
-        wlansettings.close()
+        settings["ssid"]=ssid
+        settings["password"]= password
+        print(settings)
+        a_file = open("settings.json","w")
+        json.dump(settings,a_file)
+        a_file.close()
         machine.reset()
         
         return
@@ -201,12 +230,29 @@ else:
             html = webpage()
             client.send(html)
             client.close()
+            
+
     try:
         ip = connect()
+        display.sleep(False)
+        display.fill(0)
+        display.text('Yhdista laitteen', 0, 0, 1)
+        display.text('wlan verkkoon', 0, 8, 1)
+        display.text('SSID:Picow', 0, 16, 1)
+        display.text('Salasana:', 0, 24, 1)
+        display.text('123456789', 0, 32, 1)
+        display.text('Sitten mene ', 0, 40, 1)
+        display.text('selaimella:', 0, 48, 1)
+        display.text(str(ip), 0, 56, 1)
+         
+        display.show()
+
+
         connection = open_socket(ip)
         serve(connection)
     except KeyboardInterrupt:
         machine.reset()       
-
+else:
+    print("kives")
 
 
